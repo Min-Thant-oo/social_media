9449cef3b945e16e3c711f3b24aebc954c261aa6
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-03-07 12:14:45 +0630

    Define Website home page

---
+++ resources/js/Components/app/FollowingListItems.vue
@@ -0,0 +1,181 @@
+<script setup>
+import FollowingItem from './FollowingItem.vue';
+import TextInput from '@/Components/TextInput.vue'
+import {ref} from 'vue';
+
+const searchKeyword = ref('')
+</script>
+
+<template>
+        <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-3"/>
+        <div class=" mt-3 h-[200px] lg:flex-1 overflow-auto">
+            <div v-if="false" class="text-gray-600 text-center p-3">You don't have friends yet.</div>
+            <div v-else>
+                
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="John Doe"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+                <FollowingItem
+                    image="https://source.unsplash.com/random"
+                    title="Elon Musk"
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                />
+            </div>
+        </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/GroupListItems.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import TextInput from '@/Components/TextInput.vue';
+import GroupItem from './GroupItem.vue'
+import {ref} from 'vue';
+
+const searchKeyword = ref('')
+</script>
+
+<template>
+        <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4"/>
+            <div class=" mt-3 h-[200px] lg:flex-1 overflow-auto">
+                <div v-if="false" class="text-gray-400 text-center p-3">You are not joined to any groups.</div>
+                
+                <div v-else>
+                    
+                    
+                    
+                    <GroupItem
+                        image="https://source.unsplash.com/random"
+                        title="Vue.js Developer"
+                        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                    />
+                    <GroupItem
+                        image="https://source.unsplash.com/random"
+                        title="Laravel Developer"
+                        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                    />
+                    <GroupItem
+                        image="https://source.unsplash.com/random"
+                        title="Vue.js Developer"
+                        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sed necessitatibus est?"
+                    />
+                </div>
+            </div>
+</template>
+
+<style scoped>
+
+</style>
