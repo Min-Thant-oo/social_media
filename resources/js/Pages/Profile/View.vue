b58d7fec96bc5f3559305e4b204248a462f3a823
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-03-08 22:11:59 +0630

    Implemented updatinng profile details

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -117,7 +117,7 @@ function onCoverChange(e) {
       <div class="max-w-[768px] mx-auto h-full overflow-auto">
         <!-- cover update noti -->
         <div
-          v-show="showNotification && success === 'cover-image-updated'"
+          v-show="showNotification && success"
           class="my-2 py-2 px-3 font-medium text-sm bg-emerald-500 text-white"
         >
           {{ success }}
@@ -186,7 +186,7 @@ function onCoverChange(e) {
 
                 <button 
                   v-if="!avatarImageSrc" 
-                  class="absolute left-0 top-0 right-0 bottom-0 flex items-center justify-center bg-black/50 text-gray-200 rounded-full opacity-0 group-hover/avatar:opacity-100 transition-all transform""
+                  class="absolute left-0 top-0 right-0 bottom-0 flex items-center justify-center bg-black/50 text-gray-200 rounded-full opacity-0 group-hover/avatar:opacity-100 transition-all transform"
                 >
                   <CameraIcon class="w-8 h-8"/>
 
@@ -221,7 +221,7 @@ function onCoverChange(e) {
 
             <div class="flex justify-between items-center flex-1 p-4">
               <h2 class="font-bold text-lg">{{ user.name }}</h2>
-              <PrimaryButton v-if="isMyProfile">
+              <!-- <PrimaryButton v-if="isMyProfile">
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
@@ -229,7 +229,7 @@ function onCoverChange(e) {
 
 
                 Edit Profile
-              </PrimaryButton>
+              </PrimaryButton> -->
             </div>
           </div>
 
@@ -238,9 +238,7 @@ function onCoverChange(e) {
         <div class="border-t">
           <TabGroup>
             <TabList class=" flex bg-white">
-              <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
-                <TabItem text="About" :selected="selected"/>
-              </Tab>
+              
               <Tab v-slot="{ selected }" as="template">
                 <TabItem text="Posts" :selected="selected"/>
               </Tab>
@@ -253,13 +251,12 @@ function onCoverChange(e) {
               <Tab v-slot="{ selected }" as="template">
                 <TabItem text="Photos" :selected="selected"/>
               </Tab>
+              <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
+                <TabItem text="My Profile" :selected="selected"/>
+              </Tab>
             </TabList>
       
             <TabPanels class="mt-2">
-              <TabPanel v-if="isMyProfile">
-                <Edit :must-verify-email="mustVerifyEmail" :status="status" />
-              </TabPanel>
-
               <TabPanel class="bg-white p-3 shadow">
                 Posts
               </TabPanel>
@@ -271,6 +268,9 @@ function onCoverChange(e) {
               </TabPanel>
               <TabPanel class="bg-white p-3 shadow">
                 Photos
+              </TabPanel>
+              <TabPanel v-if="isMyProfile">
+                <Edit :must-verify-email="mustVerifyEmail" :status="status" />
               </TabPanel>
             </TabPanels>
           </TabGroup>
--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/u/{user:username}', [ProfileController::class, 'index'])->name('pro
 
 Route::middleware('auth')->group(function () {
     Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])->name('profile.updateImages');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
